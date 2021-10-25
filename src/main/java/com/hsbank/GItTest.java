e37ad56afe259441e02151ae375dc591e59ffc46
kirito1028 <2398249461@qq.com>
2021-10-25 13:48:44 +0800

    second commit ignore warn

--- src/main/java/com/hsbank/GItTest.java
+++ src/main/java/com/hsbank/GItTest.java
@@ -7,5 +7,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("git-test");
         System.out.println("git-test1");
+        System.out.println("git-test2");
     }
 }
