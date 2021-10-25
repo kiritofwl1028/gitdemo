800784725599cd68763cf3b00afc5e75bb7211e0
kirito1028 <2398249461@qq.com>
2021-10-25 11:41:49 +0800

    first commit

---
+++ src/main/java/com/hsbank/GItTest.java
@@ -0,0 +1,11 @@
+package com.hsbank;
+
+/**
+ * @author kirito
+ * @create 2021-10-25 11:28
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("git-test");
+    }
+}
